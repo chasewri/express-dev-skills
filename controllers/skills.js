3bae54e1eb33e1e5367127991891600efac8c293
chasewri <chasewri@gmail.com>
2020-05-19 16:30:11 -0600

    pass titles from controller

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,13 +2,15 @@ const skillsDB = require('../models/skill')
 
 function index(req, res, next) {
   res.render('skills/index', {
-    skills: skillsDB.getAll()
+    title: 'Dev Skills List',
+    skills: skillsDB.getAll(),
   })
 }
 function show(req, res, next) {
   console.log(req.params, 'Here be those req.params you asked for');
   res.render('skills/show', {
-    skill: skillsDB.getOne(req.params.id)
+    title: 'Skill Details',
+    skill: skillsDB.getOne(req.params.id),
   })
 }
 
